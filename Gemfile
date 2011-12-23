71caf178217e890d1a17c9fffa59759da898f3a9
Zee Spencer <zspencer@zacharyspencer.com>
2011-12-23 12:54:25 -0500

    converted to scss and compass

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source :rubygems
 
 gem 'jekyll'
-gem 'ZURB-foundation'
+gem 'compass'
---
+++ config.rb
@@ -0,0 +1,9 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+
+preferred_syntax = :scss
