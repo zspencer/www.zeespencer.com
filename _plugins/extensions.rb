3ae0b5e5a8fcec42da326b7447ea133b99d8fa28
Zee Spencer <zee@zeespencer.com>
2013-03-03 14:18:24 -0500

    Add cachebuster

--- _plugins/extensions.rb
+++ _plugins/extensions.rb
@@ -5,11 +5,11 @@ require 'jekyll/tagging'
 module Jekyll
   module Filters
     def stylesheet_path(stylesheet_name)
-      "#{asset_path}/stylesheets/#{stylesheet_name}"
+      "#{asset_path}/stylesheets/#{stylesheet_name}?#{version}"
     end
 
     def javascript_path(javascript_name)
-      "#{asset_path}/javascripts/#{javascript_name}"
+      "#{asset_path}/javascripts/#{javascript_name}?#{version}"
     end
 
     def asset_path
@@ -18,6 +18,10 @@ module Jekyll
       site.config["asset_url"].find do |h|
         h.keys.include? ENV['JEKYLL_ENV']
       end.fetch(ENV['JEKYLL_ENV'], "/assets")
+    end
+
+    def version
+      Time.now.to_i
     end
   end
 end
