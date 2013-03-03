3970e753ad033429102de1ef6a5d888075a85674
Zee Spencer <zee@zeespencer.com>
2013-03-03 13:30:21 -0500

    Add Pygments for code highlighting

--- _plugins/extensions.rb
+++ _plugins/extensions.rb
@@ -3,14 +3,21 @@ require 'jekyll/tagging'
 
 
 module Jekyll
-    module Filters
-      def stylesheet_path(stylesheet_name)
-        ENV['JEKYLL_ENV'] ||= "production"
-        site = @context.registers[:site]
-        path = site.config["asset_url"].find do |h|
-          h.keys.include? ENV['JEKYLL_ENV']
-        end.fetch(ENV['JEKYLL_ENV'])
-        "#{path}/stylesheets/#{stylesheet_name}"
-      end
+  module Filters
+    def stylesheet_path(stylesheet_name)
+      "#{asset_path}/stylesheets/#{stylesheet_name}"
     end
+
+    def javascript_path(javascript_name)
+      "#{asset_path}/javascripts/#{javascript_name}"
+    end
+
+    def asset_path
+      ENV['JEKYLL_ENV'] ||= "production"
+      site = @context.registers[:site]
+      site.config["asset_url"].find do |h|
+        h.keys.include? ENV['JEKYLL_ENV']
+      end.fetch(ENV['JEKYLL_ENV'], "/assets")
+    end
+  end
 end
