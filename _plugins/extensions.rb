88b7cff1c1ba1355e75a4b0ae63b5cf792304320
Zee Spencer <zee@zeespencer.com>
2014-11-28 17:04:56 -0800

    Only run analytics in production

--- _plugins/extensions.rb
+++ _plugins/extensions.rb
@@ -8,6 +8,12 @@ module Jekyll
       input.strftime("%B %-d, %Y")
     end
   end
+
+  class EnvironmentVariablesGenerator < Generator
+    def generate(site)
+      site.config['env'] = ENV['JEKYLL_ENV'] || "development"
+    end
+  end
 end
 
 Liquid::Template.register_filter(Jekyll::DateFilter)
